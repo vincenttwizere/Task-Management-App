61f04472f15395a4b004cbca9afc38de89c778e7
vincenttwizere <v.twizere@alustudent.com>
2025-06-04 01:12:55 +0200

    adding index.html

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,9 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 5174,
-    strictPort: true // This will make Vite fail if the port is taken instead of trying another port
+    port: 3333,
+    strictPort: true, // This will make Vite fail if the port is taken instead of trying another port
+    host: true, // This enables listening on all network interfaces
+    open: true  // This will open the browser automatically
   }
 }) 
