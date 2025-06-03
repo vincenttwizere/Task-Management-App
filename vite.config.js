65b324549b72b7126681f51aa9352d28067d4fa0
vincenttwizere <v.twizere@alustudent.com>
2025-06-04 00:57:26 +0200

    adding packages

---
+++ vite.config.js
@@ -0,0 +1,11 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    port: 5173,
+    strictPort: true // This will make Vite fail if the port is taken instead of trying another port
+  }
+}) 
