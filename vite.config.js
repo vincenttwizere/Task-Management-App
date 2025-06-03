bb0319fe12d47601adcb024be357bfd68b669fb8
vincenttwizere <v.twizere@alustudent.com>
2025-06-04 01:00:58 +0200

    project configuration

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 5173,
+    port: 5174,
     strictPort: true // This will make Vite fail if the port is taken instead of trying another port
   }
 }) 
