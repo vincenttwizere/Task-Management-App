2c0273b12740919559d99392c04e6fa83a9b6fb3
vincenttwizere <v.twizere@alustudent.com>
2025-06-10 23:23:38 +0200

    latest updates

--- src/contexts/AuthContext.jsx
+++ src/contexts/AuthContext.jsx
@@ -108,7 +108,7 @@ export function AuthProvider({ children }) {
 
   return (
     <AuthContext.Provider value={value}>
-      {!loading && children}
+      {children}
     </AuthContext.Provider>
   );
 } 
