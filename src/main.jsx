a3aec76abdb08efee44f58bb6460fcc887bea0d1
vincenttwizere <v.twizere@alustudent.com>
2025-06-24 10:12:40 +0200

    updates

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter as Router } from 'react-router-dom';
 import App from './App';
 import './index.css';
 
@@ -9,7 +10,9 @@ if (root) {
   const reactRoot = ReactDOM.createRoot(root);
   reactRoot.render(
     <React.StrictMode>
-      <App />
+      <Router>
+        <App />
+      </Router>
     </React.StrictMode>
   );
 }
